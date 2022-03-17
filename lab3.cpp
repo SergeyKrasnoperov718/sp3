3e00245675f1f4644a4f146534ad2d8f1725c463
SergeyKrasnoperov718 <100361390+SergeyKrasnoperov718@users.noreply.github.com>
2022-03-17 20:30:29 +0700

    Update lab3.cpp

--- lab3.cpp
+++ lab3.cpp
@@ -12,23 +12,3 @@ int main()
  cout<<endl;
  return 0;
 }
-
-
-"mov %[n_a], %%rax        \n\t"
-   "start:                   \n\t"
-    "push (%%rax)            \n\t"
-    "add $8, %%rax           \n\t"
-    "cmp %[k_a], %%rax       \n\t"
-    "jne start               \n\t"
-    //"add $-72, %%rax           \n\t"
-    "mov %[n_a], %%eax       \n\t"
-   "loop:                    \n\t"
-    "pop (%%rax)             \n\t"
-    "add $8, %%rax           \n\t"
-    "cmp %[k_a], %%rax       \n\t"
-    "jne loop                \n\t"
-   "end_asm:                 \n\t"
-    :
-    : [n_a]"m"(n_a),
-      [k_a]"m"(k_a)
-    : "%rax"
